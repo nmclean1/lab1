46ca55412b4a72ec44b3e25411e99e63acac39cc
Jacob Spiegel <jsamuelspiegel@gmail.com>
2018-10-04 15:03:09 -0500

    added keys

--- java/src/edu/virginia/lab1test/LabOneGame.java
+++ java/src/edu/virginia/lab1test/LabOneGame.java
@@ -1,6 +1,7 @@
 package edu.virginia.lab1test;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import edu.virginia.engine.display.Game;
@@ -32,6 +33,76 @@ public class LabOneGame extends Game{
 		
 		/* Make sure mario is not null. Sometimes Swing can auto cause an extra frame to go before everything is initialized */
 		if(mario != null) mario.update(pressedKeys);
+
+		if (pressedKeys.contains(KeyEvent.VK_UP)){
+			mario.setPosition(new Point(mario.getPosition().x,
+					mario.getPosition().y - 5));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_DOWN)){
+			mario.setPosition(new Point(mario.getPosition().x,
+					mario.getPosition().y + 5));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_LEFT)){
+			mario.setPosition(new Point(mario.getPosition().x - 5,
+					mario.getPosition().y));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_RIGHT)){
+			mario.setPosition(new Point(mario.getPosition().x + 5,
+					mario.getPosition().y));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_I)){
+			mario.setPivotPoint(new Point(mario.getPivotPoint().x,
+					mario.getPivotPoint().y - 5));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_K)){
+			mario.setPivotPoint(new Point(mario.getPivotPoint().x,
+					mario.getPivotPoint().y + 5));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_J)){
+			mario.setPivotPoint(new Point(mario.getPivotPoint().x - 5,
+					mario.getPivotPoint().y));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_L)){
+			mario.setPivotPoint(new Point(mario.getPivotPoint().x + 5,
+					mario.getPivotPoint().y));
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_W)){
+			mario.setRotation(mario.getRotation() + 5);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_Q)){
+			mario.setRotation(mario.getRotation() - 5);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_V)){
+			mario.setVisible(mario.getVisible() ? false : true);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_Z)){
+			mario.setAlpha(mario.getAlpha() - 0.1f);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_X)){
+			mario.setAlpha(mario.getAlpha() + 0.1f);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_A)){
+			mario.setScaleX(mario.getScaleX() - 0.1);
+			mario.setScaleY(mario.getScaleY() - 0.1);
+		}
+
+		if (pressedKeys.contains(KeyEvent.VK_S)){
+			mario.setScaleX(mario.getScaleX() + 0.1);
+			mario.setScaleY(mario.getScaleY() + 0.1);
+		}
 		
 	}
 	
