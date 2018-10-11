1678d3435462aec691c4b062e48e34ff7bc7878e
Noah McLean <nmclean@uchicago.edu>
2018-10-11 14:12:29 -0500

    adding pre-lab2

--- java/src/edu/virginia/engine/display/Sprite.java
+++ java/src/edu/virginia/engine/display/Sprite.java
@@ -1,6 +1,8 @@
 package edu.virginia.engine.display;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Timer;
 
 /**
  * Nothing in this class (yet) because there is nothing specific to a Sprite yet that a DisplayObject
@@ -19,5 +21,44 @@ public class Sprite extends DisplayObject {
 	@Override
 	public void update(ArrayList<Integer> pressedKeys) {
 		super.update(pressedKeys);
+	}
+}
+
+public class AnimatedSprite extends Sprite {
+
+	private ArrayList<animation> animations;
+	private boolean playing;
+	private String fileName;
+	private ArrayList<BufferedImage> frames;
+	private int currentFrame;
+	private int startFrame;
+	private int endFrame;
+	private static final int DEFAULT_ANIMATION_SPEED = 1;
+	private int animationSpeed;
+	private Timer gameClock;
+
+	public AnimatedSprite(String id, String fileName, int position) {
+		super(id);
+		this.setFileName(fileName);
+		this.setPosition(position);
+		this.setGameClock();
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	@Override
+	public void setPosition(Point pos) {
+		super.setPosition(pos);
+	}
+
+	public void setGameClock() {
+		this.gameClock = new Timer(DEFAULT_ANIMATION_SPEED, this);
+	}
+
+	public void initGameClock() {
+		if (this.gameClock == null)
+			setGameClock();
 	}
 }
